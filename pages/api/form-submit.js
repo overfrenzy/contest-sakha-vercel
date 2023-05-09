966f091397e09f6372abe6ec9bf890ddcf42ad1f
overfrenzy <xplo1d387@gmail.com>
2023-05-10 00:21:41 +0900

    final possible fix for database insert

--- pages/api/form-submit.js
+++ pages/api/form-submit.js
@@ -18,11 +18,14 @@ export default async function handler(req, res) {
   try {
     await client.connect();
 
+    // Create the Participant table if it does not exist
+    await client.query(
+      "CREATE TABLE IF NOT EXISTS Participant (ID INT PRIMARY KEY DEFAULT unique_rowid(), Name STRING)"
+    );
+
     // Insert the data into the Participant table
     const { name } = req.body;
-    await client.query("INSERT INTO Participant (Name) VALUES (\$1)", [
-      name,
-    ]);
+    await client.query("INSERT INTO Participant (Name) VALUES ($1)", [name]);
 
     res.status(200).json({ message: "Data inserted successfully" });
 
